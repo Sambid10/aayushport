083b68a68f719c30faf263f8cab49a7d78cb6fb8
Sambid10 <sambidshk10@gmail.com>
2025-01-14 14:41:16 +0545

    tab ko problem thyi

--- src/app/all/page.tsx
+++ src/app/all/page.tsx
@@ -44,38 +44,32 @@ export default function Page() {
             open: { y: "0%" }
           }}
           transition={{ duration: 0.2 }}
-          className="flex justify-center  items-center sticky top-0 pt-3 z-50 py-1"
+          className="flex justify-center   items-center sticky top-0 pt-3 z-50 py-1"
         >
-          <TabsList className="grid w-[50%] h-[4.2rem] grid-cols-2 mb-10  border border-gray-800 bg-gray-900 text-[#fff] rounded-full">
-            <motion.div
-
-              className="flex justify-center gap-2 items-center h-full"
-            >
+          <TabsList className="w-fit h-[4.2rem]  px-2 flex items-center gap-2 mb-10  border border-gray-800 bg-gray-900 text-[#fff] rounded-full">
+           
               <button
                   onClick={handleBack} 
-                className='px-4 hover:bg-gray-800 rounded-full h-[100%] transition-colors ease-in duration-200 flex justify-center items-center'
+                className='px-2 py-2 hover:bg-gray-800 rounded-full  transition-colors ease-in duration-200 flex justify-center items-center'
               >
                 <BiChevronLeft className='h-7 w-7 '/>
               </button>
               <TabsTrigger
-                className="text-lg text-gray-200 font-medium h-[80%]  rounded-full w-full hover:bg-gray-700 ease-in duration-200 transition-colors"
+                className="text-lg px-4 text-gray-200 font-medium h-[80%]  rounded-full w-fit hover:bg-gray-700 ease-in duration-200 transition-colors"
                 value="Poster"
               >
                 Poster Designs
               </TabsTrigger>
-            </motion.div>
+            
 
-            <motion.div
-
-              className="flex justify-center items-center h-full"
-            >
+             
               <TabsTrigger
-                className="text-lg text-gray-200 font-medium h-[80%]  w-full rounded-full  hover:bg-gray-700 ease-in duration-200 transition-colors"
+                className="text-lg px-4 text-gray-200 font-medium h-[80%]  w-fit rounded-full  hover:bg-gray-700 ease-in duration-200 transition-colors"
                 value="tee-designs"
               >
                 Tee Designs
               </TabsTrigger>
-            </motion.div>
+           
           </TabsList>
         </motion.div>
 
